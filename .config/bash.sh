acf40c005a295c6c191dd05f2507a3760484b66f
James O'Brien <bosemaj@gmail.com>
2025-08-14 23:46:35 -0400

    Split bash and zsh custom config

---
+++ .config/bash.sh
@@ -0,0 +1,6 @@
+# A Bash counterpart to the .zsh version of this file.
+# Overrides settings that I want to change in ubuntu
+export HISTFILE=~/.bash_history
+export HISTSIZE=999999999 # I think I need this to be a real number
+export HISTFILESIZE=$HISTSIZE
+
